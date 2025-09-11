3d7b441d6e4c1a509a8746eb7216a016e36b8f72
Rana-Awais-khan <browserlogin411@gmail.com>
2025-09-11 11:50:27 +0500

    message

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -1,7 +1,15 @@
+    const username = document.getElementById('name').value;
+    const age = document.getElementById('age').value;
+    const email = document.getElementById('email').value;
+    const job = document.getElementById('job').value;
+    const designation = document.getElementById('designation').value;
+    const productType = document.getElementById('productType').value;
+    const feedback = document.getElementById('feedbackText').value;
+
+
+
 function submitFeedback() {
    alert('Thank you for your valuable feedback')
-   document.getElementById('userInfo').style.display = 'block';
-
    
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
@@ -11,24 +19,15 @@ function submitFeedback() {
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
     
-    
+    document.getElementById('userInfo').style.display = 'block';
 }
 
-
-    const username = document.getElementById('name').value;
-    const age = document.getElementById('age').value;
-    const email = document.getElementById('email').value;
-    const job = document.getElementById('job').value;
-    const designation = document.getElementById('designation').value;
-    const productType = document.getElementById('productType').value;
-    const feedback = document.getElementById('feedbackText').value;
-   
-   
-    const submitButton=document.getElementById('submitBtn');
-    submitButton.onclick = submitFeedback;
+const submitButton=document.getElementById('submitBtn');
+submitButton.onclick = submitFeedback;
  
 document.addEventListener('keydown', function(event) {
    if (event.key === 'Enter') {
+    event.preventDefault();
      submitFeedback();
    }
  });
